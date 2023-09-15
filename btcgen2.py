f27e69a7ad120fffbe683ee16c41c107a6dcbdd8
petasplit <61479922+petasplit@users.noreply.github.com>
2023-09-15 18:05:45 +0200

    Update btcgen2.py

--- btcgen2.py
+++ btcgen2.py
@@ -94,7 +94,7 @@ def worker(queue):
 def process(data):
     private_key = data[0]
     address = data[1]
-    urlblock = "https://api.blockcypher.com/v1/btc/main/addrs/" + str(address)
+    urlblock = "https://bitcoin.atomicwallet.io/address/" + str(address)
     respone_block = requests.get(urlblock)
     byte_string = respone_block.content
     source_code = html.fromstring(byte_string)
